e3b30f924bb8373e795056e26bb743ff2cf769cf
i.bergonyo <i.bergonyo_xsr@ereuse.org>
2018-08-02 02:53:53 +0200

    Yeaaaaaaah!

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-
+  $("#front").click(function(){
+    $("#panel_front").slideToggle("slow");
+    $("#panel_back").slideUp("slow");
+    $("#panel_other").slideUp("slow");
+  });
+  $("#back").click(function(){
+    $("#panel_back").slideToggle("slow");
+    $("#panel_front").slideUp("slow");
+    $("#panel_other").slideUp("slow");
+  });
+  $("#other").click(function(){
+    $("#panel_other").slideToggle("slow");
+    $("#panel_back").slideUp("slow");
+    $("#panel_front").slideUp("slow");
+  });
 });
